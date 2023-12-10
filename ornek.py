fac5e4ff3a274d11315376d06874a6ff8bec7e91
Şenol ÇAVDAR <senol-hasretim@windowslive.com>
2023-12-10 00:09:53 +0000

    ornek.py updated

---
+++ ornek.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import pandas as pd
+
+app = Flask(__name__)
+api = Api(app)
+
+class Users(Resource):
+    def get(self):
+        data = pd.read_csv('says.csv')
+        data = data.to_dict('records')
+        return {'data' : data}, 200
+
+    def post(self):
+        date=request.args['date']
+        says=request.args['says']
+        topic=request.args['topic']
+
+        data = pd.read_csv('says.csv')
+
+        new_data = pd.DataFrame({
+            'date'    : ['date'],
+            'says'    : ['says'],
+            'topic'      : ['topic']
+        })
+        data = data.append(new_data, ignore_index = True)
+        data.to_csv('users.csv', index=False)
+        return {'data' : new_data.to_dict('records')}, 200
+
+class Name(Resource):
+    def get(self,date):
+        data = pd.read_csv('')
+        data = data.to_dict('records')
+        for entry in data:
+            if entry['date'] == date :
+                return {'data' : entry}, 200
+        return {'message' : 'Girilen tarihte bir söz bulunamadı !'}, 404
+
+
+
+# Add URL endpoints
+api.add_resource(Says, '/says')
+api.add_resource(Date, '/<string:date>')
+
+
+if __name__ == '__main__':
+    # app.run(host="0.0.0.0", port=6767)
+    app.run()
